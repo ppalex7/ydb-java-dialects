087946a588d13e4f4610be12c68a62802ef98472
Aleksandr Duplishchev <ppalex@yandex-team.ru>
2026-02-25 09:54:21 +0300

    replace-update test

--- jooq-dialect/src/test/java/tech/ydb/jooq/BatchReplaceTest.java
+++ jooq-dialect/src/test/java/tech/ydb/jooq/BatchReplaceTest.java
@@ -9,11 +9,12 @@ import org.junit.jupiter.api.Test;
 
 import static jooq.generated.ydb.default_schema.Tables.SERIES;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class BatchReplaceTest extends BaseTest {
 
     @Test
-    public void testSimpleInsertViaBatchUpsert() {
+    public void testSimpleInsertViaBatchReplace() {
         SeriesRecord newRecord = new SeriesRecord();
         newRecord.setSeriesId(ULong.valueOf(1));
         newRecord.setTitle("New Series");
@@ -31,7 +32,27 @@ public class BatchReplaceTest extends BaseTest {
     }
 
     @Test
-    public void testMultipleInsertViaBatchUpsert() {
+    public void testSimpleUpdateViaBatchReplace() {
+        dsl.batchInsert(getExampleRecords()).execute();
+
+        SeriesRecord replaceRecord = new SeriesRecord(ULong.valueOf(1), "Updated Series One", "Updated Info One",
+                ULong.valueOf(20220201));
+
+        dsl.batchReplace(replaceRecord)
+                .execute();
+
+        SeriesRecord updatedRecord = dsl.selectFrom(SERIES)
+                .where(SERIES.SERIES_ID.eq(ULong.valueOf(1)))
+                .fetchOne();
+
+        assertNotNull(updatedRecord);
+        assertEquals("Updated Series One", updatedRecord.getTitle());
+        assertEquals("Updated Info One", updatedRecord.getSeriesInfo());
+        assertEquals(ULong.valueOf(20220201), updatedRecord.getReleaseDate());
+    }
+
+    @Test
+    public void testMultipleInsertViaBatchReplace() {
         List<SeriesRecord> records = getExampleRecords();
 
         dsl.batchReplace(records)
